8cc000eadc55a554dbdd328a5ecc3fb38f669863
karan <karansalunkhe88@gmail.com>
2024-10-06 14:14:23 +0530

    gtags added

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+declare var gtag: Function;
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  trackButtonClickOne() {
+    gtag('event', 'click', {
+      event_category: 'Button',
+      event_label: 'Click Me Button home',
+      value: 1,
+    });
   }
 
+  trackButtonClickTwo() {
+    gtag('event', 'click', {
+      event_category: 'Button',
+      event_label: 'Click Me Button about',
+      value: 1,
+    });
+  }
 }
